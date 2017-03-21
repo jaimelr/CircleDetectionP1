79f83f9ca5fe668d3f7a7d7d6ef66a8c4bfc53ec
NatanaelCastillo <natanael.cp19@gmail.com>
2017-03-20 22:02:53 -0600

    modificacion de main

--- main.c
+++ main.c
@@ -11,37 +11,56 @@
 
 #include "PDI.h"
 #include "figura.h"
+//#include "PSO.h"
 
 int main(void)
 { 
 	//Declarar un Puntero a imagen
 	gcIMG *Img1;
-	//unsigned int i,j;
+	unsigned int i,j;
 	unsigned int W,H;
-	W=300;
-	H=300;
+	//unsigned int x,y;
+	//W=300;
+	//H=300;
 	//Abrir una imagen llamada Uno.bmp
-	Img1=gcGetImgBmp("imagen.bmp");
+	Img1=gcGetImgBmp("Circulos.bmp");
+	H=Img1->alto; //Obtener alto
+	W=Img1->ancho;//Obtener ancho
+	printf("\nalto\t%u",H);
+	printf("\nancho\t%u",W);
 	//Crear una imagen nueva
 	//Img1=gcNewImg(H,W);
-	/*  
-	//Obtiene sus dimensiones
-	printf("\n El ancho es: %i",Img1->ancho);
-	printf("\n El alto es: %i",Img1->alto);
-	printf("\n El tama~no es: %i",Img1->size);
-	printf("\n\n");
-	*/
 	
-	//Pintar toda la imagen de un color
-	/*
+	/* //Pintar toda la imagen de un color
 	for(i=0; i<H; i++)
 		for(j=0;j<W; j++)
 			Img1->imx[i*Img1->ancho+j]=0;
 	*/
+	
 	//Dibujar circulo
-	circulo(Img1,H,W);
+	//circulo(Img1,H,W);
+	
 	//Crear archivo de imagen
-	gcPutImgBmp("imagen.bmp",Img1);
+	//gcPutImgBmp("imagen.bmp",Img1);
+	
+	//Barrer imagen buscando puntos negros
+	unsigned int k=0;
+	int vector[H*W];
+	for(i=0; i<H; i++)
+	{
+		for(j=0;j<W; j++)
+		{
+			if(Img1->imx[i*Img1->ancho+j] <= 10)
+			{
+				vector[k]=i*Img1->ancho+j;
+				//printf("\n\n%i",vector[k]);
+				k++;
+				printf("\n\nX=%u\ty=%u",i,j);
+			}
+			//printf("\n\n%e",Img1->imx[i*Img1->ancho+j]);
+		}
+	}
+	
 	//Libera la Imagen utilizada
 	gcFreeImg(Img1);
 	return 0;
